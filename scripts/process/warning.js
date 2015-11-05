a14e5911cab930bc0ada5dbe3095aa4fcc66f61e
Rafe Lepre <rafe.lepre@skedge.me>
2015-11-05 15:57:53 -0500

    added the ability for a detailed warning log

--- scripts/process/warning.js
+++ scripts/process/warning.js
@@ -1,11 +1,15 @@
-module.exports = ( function ( printMessage , theme , stringAlignRight ) {
+module.exports = ( function ( printMessage , theme , stringAlignRight , _ ) {
 
-	return function warning ( message , note ) {
+	return function warning ( message , note , err ) {
 
-		printMessage( theme.warning( stringAlignRight( 'Warning' ) ) , message , note );
+		printMessage(	theme.warning( stringAlignRight( 'Warning' ) ),
+						_.isUndefined( err ) ?
+						message :
+						{ title : message , message : err  } , note );
 
 	};
 
 } ) (	require( './../util/printMessage.js' ),
 		require( './../util/theme.js' ),
-		require( './../util/stringAlignRight.js' ) );
+		require( './../util/stringAlignRight.js' ),
+		require( 'underscore' ) );
